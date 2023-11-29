4b3f73907d1c5f915dc18ffa094e91eb4035da9d
Illia <ilyachernykan@gmail.com>
2023-11-29 23:04:56 +0200

    Add server

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const { Message } = require("./models");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res, next) => {
+  try {
+    const message = await Message.find();
+    res.status(200).send({ data: message });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((err, req, res, next) => {
+  return res.status(500).send({ errors: [err] });
+});
+
+module.exports = app;
---
+++ server/configs/mongo.js
@@ -0,0 +1,7 @@
+module.exports = {
+  development: {
+    host: "localhost",
+    port: 27017,
+    dbName: "fm_chat",
+  },
+};
